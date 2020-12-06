ac377e839707fe66f9a78174926bdf9ea3c75f66
PatriciaMM10 <75554000+PatriciaMM10@users.noreply.github.com>
2020-12-06 14:24:07 -0600

    Add files via upload

--- Base_SI.php
+++ Base_SI.php
@@ -1,8 +1,8 @@
 <html> 
  <body>  
  <h1> Uso de la Estructura de Control SI en PHP y HTML </h1>
- <form action="Uso_si.php" method="post">      
- Balance Bancario: <input type="text" name="bank_balance <br>"><br>      
+ <form action="Uso_SI.php" method="post">      
+ Balance Bancario: <input type="text" name="bank_balance"> <br>  
  <input type="submit">    
  </form>
 </body>
